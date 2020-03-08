2cdbf44e3f980b439676e3c4146e36e1a633d3f7
stadnikivan <39565192+stadnikivan@users.noreply.github.com>
2020-03-08 06:14:12 +0200

    Update index.tsx

--- src/Timer/index.tsx
+++ src/Timer/index.tsx
@@ -7,25 +7,36 @@ interface State {
 
 export default class Timer extends React.Component<{}, State>  {
   timer: any
+
     constructor(props: never) {
         super(props);
         this.state = {
             time: 0,
             start: Date.now()
         }
+        console.log("constructor")
     }
 
     componentDidMount() { 
+        console.log("Запуск метода монтирования")
         this.timer = setInterval(() => this.setState({
             time: Date.now() - this.state.start
         }), 1000);
     }
 
+    
     shouldComponentUpdate(props:{}, nextState: State) {
+        console.log("Метод shouldComponentUpdat вызывается перед рендером")
         return nextState !== this.state;
+    }
+        
+    
+    componentDidUpdate(props:{}, prevState: State) {
+        console.log("Метод componentDidUpdate вызывается при обновлении")
     }
       
     componentWillUnmount() {
+        console.log("Запуск метода размонтирования(очистка)")
         clearInterval(this.timer);
     }
 
@@ -35,7 +46,6 @@ export default class Timer extends React.Component<{}, State>  {
         date.setHours(date.getHours() - 3)
         return date.toLocaleTimeString()
     }
-
     render() {
         return (
             <div>
